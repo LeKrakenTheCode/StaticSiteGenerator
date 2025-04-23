ceb921304ad0d00e4297eaf08ce79496a7da6871
tmcmillan@tmcmillan.net <tmcmillan@tmcmillan.net>
2025-04-23 10:52:37 -0500

    ch2 L4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -11,5 +11,13 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsNotNone(node2.props_to_html())
         self.assertEqual(node1.props_to_html(), "")
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
 if __name__ == "__main__":
     unittest.main()
